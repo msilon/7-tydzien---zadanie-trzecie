242ad6ef44eae6845cd2f2047c7d9bf3c927bd3b
msilon <willardan@gmail.com>
2017-02-21 20:05:17 +0100

    Zadanie trzecie

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Podaj wartość podstawy trójkąta.'),
+	h = prompt('Podaj wartość wysokości trójkąta.')
+	triangleArea = a*h/2;
+	alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+	console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
